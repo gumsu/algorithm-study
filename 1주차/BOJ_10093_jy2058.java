60cbccf626b2f2cc094bf26c0927bde85528c6d0
kjy <jiy3285@gmail.com>
2021-09-06 22:13:00 +0900

    10093 숫자

--- 1주차/BOJ_10093_jy2058.java
+++ 1주차/BOJ_10093_jy2058.java
@@ -1,5 +1,30 @@
+import org.omg.Messaging.SyncScopeHelper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BOJ_10093_jy2058 {
-    public static void main(String[] args) {
-        System.out.println("테스트");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long start = 0, end = 0;
+        if (A > B){
+            System.out.println(A-B-1);
+            start = B;
+            end = A;
+        }else if(A < B){
+            System.out.println(B-A-1);
+            start = A;
+            end = B;
+        }else{
+            System.out.println(0);
+        }
+        for (long i = start + 1; i < end; i++) {
+            System.out.print(i + " ");
+        }
     }
 }
