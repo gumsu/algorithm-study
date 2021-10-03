98a7817ef5c688a74c90b4e07030bb9ec237ada2
Jade Kim <jadek.engr@gmail.com>
2021-10-03 12:59:18 +0900

    monad 21-10-03 ++

--- 4주차/도로와신호등/도로와신호등_monad.py
+++ 4주차/도로와신호등/도로와신호등_monad.py
@@ -2,20 +2,19 @@ n, l = map(int, input().split())
 lights = []
 for _ in range(n):
     lights.append(list(map(int, input().split())))
-
+# ------------------------
 time = 0  # 시간
-move = 0  # 이동한 거리
+
 for i in range(n):
-    b = lights[i][0] if i == 0 else lights[i][0] - lights[i-1][0]
+    b = lights[i][0] if i == 0 else lights[i][0]-lights[i-1][0]  # 전 신호등과의 사이거리
     r = lights[i][1]
     g = lights[i][2]
 
     time += b
-    move += b
 
     tmp_time = time % (r+g)  # 신호가 여러번 바뀐 시간을 뺀 시간 - 임시 변수
-    if tmp_time < r:
-        time += r-tmp_time
+    if tmp_time < r:  # 아직 빨간 불이라서 기다려야 함
+        time += r-tmp_time  # 기다려야 하는 시간 더하기
 
 time += l-lights[n-1][0]
 
--- 4주차/트리의부모찾기/트리의부모찾기_monad.py
+++ 4주차/트리의부모찾기/트리의부모찾기_monad.py
@@ -1,15 +1,18 @@
 # 채점 왜이렇게 오래 걸려,,,
+# 또 런타임 에러 ...
+
 import sys
 sys.setrecursionlimit(100000)
 
 n = int(input())
 
 graph = [[] for _ in range(n)]
+
 for i in range(n-1):
     a, b = map(int, input().split())
     graph[a-1].append(b)
     graph[b-1].append(a)
-
+# -------------------------
 parents = [0]*n
 visited = [False]*n
 
@@ -26,3 +29,5 @@ dfs(1)
 
 for i in range(1, n):
     print(parents[i])
+
+# 리스트 인덱스가 헷갈림
