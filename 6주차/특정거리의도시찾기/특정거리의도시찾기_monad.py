c130fca9786630fd02ce07b55914e38963b5f933
Jade Kim <jadek.engr@gmail.com>
2021-10-24 14:11:54 +0900

    monad

--- 6주차/특정거리의도시찾기/특정거리의도시찾기_monad.py
+++ 6주차/특정거리의도시찾기/특정거리의도시찾기_monad.py
@@ -7,20 +7,20 @@ for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
 
-visited = [-1]*(n+1)
-visited[x] = 0
+distance = [-1]*(n+1)
+distance[x] = 0
 
 q = deque([x])
-while q:
+while q:  # q가 남아있는 동안 지속
     now = q.popleft()
     for next in graph[now]:
-        if visited[next] == -1:
+        if distance[next] == -1:  # 방문 안한 노드
             q.append(next)
-            visited[next] = visited[now]+1
+            distance[next] = distance[now]+1
 
 exist = False
 for i in range(1, n+1):
-    if visited[i] == k:
+    if distance[i] == k:
         print(i)
         exist = True
 
